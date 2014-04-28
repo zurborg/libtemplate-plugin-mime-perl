8f701f87261cff874b81b38dc2219ca148ebaf88
David Zurborg <d.zurborg@propartner-it.de>
2014-04-28 17:31:17 +0200

    updated documentation

--- lib/Template/Plugin/MIME.pm
+++ lib/Template/Plugin/MIME.pm
@@ -78,11 +78,148 @@ sub base64($$) {
     return encode_base64($_[1]);
 }
 
+=head1 METHODS FOR USE OUTSIDE TEMPLATE
+
+=cut
+
+=head2 C<< attachments() >>
+
+Returns all attached files.
+
+    use Template;
+    use Template::Plugin::MIME;
+    
+    $template = Template->new;
+    $template->process(...);
+    
+    $attachments = Template::Plugin::MIME->attachments($template);
+
+=cut
+
 sub attachments($$) {
     my ($self, $template) = @_;
     my $context = $template->context;
     return $context->{$NAME}->{attachments}->{all};
 }
+
+=head2 C<< merge($template, $mail) >>
+
+This method merges a L<MIME::Entity|MIME::Entity> documents together with all attached files in the template.
+
+    use Template;
+    use Template::Plugin::MIME;
+    use MIME::Entity;
+    
+    $template = Template->new;
+    $template->process($infile, $stash, $outfile);
+    
+    $entity = MIME::Entity->build(
+        From => ...,
+        To => ...,
+        Subject => ...,
+        Type => 'text/html',
+        Path => $outfile,
+    );
+    
+    Template::Plugin::MIME->merge($template, $entity);
+        # $entity is now multipart/related
+
+This methods invokes C< make_multipart('related') > on C< $entity > an then attaches all party to this entity with C< add_part() >.
+
+A more complex example is shown below. This can be used when you want seperate attachement dispositions together:
+
+    use Template;
+    use Template::Plugin::MIME;
+    use MIME::Entity;
+    
+    $template = Template->new;
+    $template->process($ttfile, $stash, $outfile);
+    
+    $inner_text = MIME::Entity->build(
+        Top => 0, # this is very important!
+        Type => 'text/plain',
+        Path => $plainfile,
+    );
+    
+    $inner_html = MIME::Entity->build(
+        Top => 0, # this is very important!
+        Type => 'text/html',
+        Path => $outfile,
+    );
+    
+    $outer = MIME::Entity->build(
+        From => ...,
+        To => ...,
+        Subject => ...,
+        Type => 'multipart/alternative',
+    );
+    
+    # first, merges the attachments into the html entity:
+    Template::Plugin::MIME->merge($template, $inner_html);
+        # $inner_html is now multipart/related
+    
+    # seconds merges the alternative into the root entity:
+    $outer->add_part($inner_text);
+    $outer->add_part($inner_html);
+
+The advantage is, the root entity considers of two alternative: a plain text and a html variant. the html variant is a multipart too, with related content (images, ...) attached.
+
+And a total complex example shows how to use mixed content:
+
+    use Template;
+    use Template::Plugin::MIME;
+    use MIME::Entity;
+    
+    $template = Template->new;
+    $template->process($ttfile, $stash, $outfile);
+    
+    $inner_text = MIME::Entity->build(
+        Top => 0, # this is very important!
+        Type => 'text/plain',
+        Path => $plainfile,
+    );
+    
+    $inner_html = MIME::Entity->build(
+        Top => 0, # this is very important!
+        Type => 'text/html',
+        Path => $outfile,
+    );
+    
+    $outer = MIME::Entity->build(
+        Top => 0, # this is very important!
+        Type => 'multipart/alternative',
+    );
+    
+    $entity = MIME::Entity->build(
+        From => ...,
+        To => ...,
+        Subject => ...,
+        Type => 'multipart/mixed',
+    );
+    
+    # first, merge the attachments into the html entity:
+    Template::Plugin::MIME->merge($template, $inner_html);
+        # $inner_html is now multipart/related
+    
+    # second, merge the alternative into the outer entity:
+    $outer->add_part($inner_text);
+    $outer->add_part($inner_html);
+    
+    # third, merge all parts together the root entity
+    $entity->add_part($outer);
+    $entity->add_part(Path => 'invoice.pdf', Type => 'application/pdf', Filename => 'Your Invoice.pdf');
+
+The mime structue is now
+
+    (root) multipart/mixed
+    |-> multipart/alternative
+    |   |-> multipart/related
+    |   |   |-> text/html
+    |   |   `-> image/png
+    |   `-> text/plain
+    `-> application/pdf
+
+=cut
 
 sub merge {
     my ($self, $template, $mail) = @_;
@@ -97,10 +234,44 @@ sub merge {
     
     return $mail;
 }
-    
-=head1 FUNCTIONS
+   
+=head1 FUNCTIONS FOR USE INSIDE TEMPLATE
 
-=head2 insert($path [, $mimetype] )
+=head2 C<< attach($path [, %options] ) >>
+
+This method attaches a file and returns a Content-Id for use within html content, for example.
+
+    [%  USE mail = MIME %]
+    
+    [% signature_cid = mail.attach("signature.png") %]
+    
+    <img src="cid:[% signature_cid %]" />
+
+The paramhash C<%options> is equivalent to the C<build> class/instance method in L<MIME::Entity|MIME::Entity>. The following options are overridden in order to work with related content:
+
+=over 4
+
+=item * C< Path > is equivalent to C< $path >
+
+=item * C< Id > is the content-id, automatically generated.
+
+=item * C< Encoding > is forced to Base64.
+
+=item * C< Type > is the content type (but see below for more information)
+
+=item * C< Top > is 0, since an attachment is not a top-level entity.
+
+=back
+
+All other options are passed through.
+
+=head3 Obtaining Content-Type
+
+If the Options C< Type > is set, this will be used regardless what the file is really is.
+
+If L<File::LibMagic|File::LibMagic> is installed on your system, C<checktype_filename> will be invoked to obtain the mime-type. This method may fail and error messages are discarded for now.
+
+If all fails, the mime-type "application/octet-stream" is used.
 
 =cut
 
