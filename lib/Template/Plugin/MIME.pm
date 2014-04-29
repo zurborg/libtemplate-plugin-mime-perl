c5057797fe4595c90a689a3543ec8f7c3330cb3c
David Zurborg <d.zurborg@propartner-it.de>
2014-04-29 15:45:07 +0200

    bump to version 0.12

--- lib/Template/Plugin/MIME.pm
+++ lib/Template/Plugin/MIME.pm
@@ -24,11 +24,11 @@ Template::Plugin::MIME - TemplateToolkit plugin providing a interface to MIME::E
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 our $NAME = __PACKAGE__;
 
